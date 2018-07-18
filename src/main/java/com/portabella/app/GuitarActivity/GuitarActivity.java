2c3ce8c4df59aeac18466d09885eb5684fff28fe
Omer Rom <omerom88@gmail.com>
2018-07-18 20:49:25 +0300

    ready for check _

--- src/main/java/com/portabella/app/GuitarActivity/GuitarActivity.java
+++ src/main/java/com/portabella/app/GuitarActivity/GuitarActivity.java
@@ -193,6 +193,7 @@ public class GuitarActivity extends Activity {
         }
     }
 
+
     @Subscribe
     public void writeDataFromArduino(WritingEvent event) {
         textFromArduino.setText(event.getText());
